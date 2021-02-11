0f1b63ae170bf23b85f173b2f7b7c47ef2e8f591
Alyssa Arias <alyssaarias27@gmail.com>
2021-02-11 19:29:16 +0800

    with user input & rotation points

--- Assets/Scripts/TetroBlock.cs
+++ Assets/Scripts/TetroBlock.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TetroBlock : MonoBehaviour
 {
+    private float prevTime;
+    public float fallTime = 0.8f;
+
+    public Vector3 rotPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,30 @@ public class TetroBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckUserInput();
     }
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            transform.RotateAround(transform.TransformPoint(rotPoint), new Vector3(0, 0, 1), 90);
+        }
+
+        else if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) || Time.time - prevTime >= fallTime)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            prevTime = Time.time;
+        }
+     }
 }
