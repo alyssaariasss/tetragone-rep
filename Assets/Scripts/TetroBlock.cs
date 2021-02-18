07f2197828a4117f585c6033e3dbb249412ee747
Shiara-Deudor <58914210+Shiara-Deudor@users.noreply.github.com>
2021-02-18 14:36:09 +0800

    Grid limits & clear lines

--- Assets/Scripts/TetroBlock.cs
+++ Assets/Scripts/TetroBlock.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TetroBlock : MonoBehaviour
 {
+
     private float prevTime;
     public float fallTime = 0.8f;
-
+    public static int height = 20;
+    public static int width = 10;
     public Vector3 rotPoint;
+    private static Transform[,] grid = new Transform[width, height];
+    
+
+  
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -26,11 +33,15 @@ public class TetroBlock : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
         }
 
         else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(1, 0, 0);
         }
 
         else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
@@ -41,7 +52,96 @@ public class TetroBlock : MonoBehaviour
         else if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) || Time.time - prevTime >= fallTime)
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+                this.enabled = false;
+                FindObjectOfType<SpawnTetromino>().NewTetromino();
+            }
+            
             prevTime = Time.time;
         }
-     }
+    }
+
+    void CheckForLines()
+    {
+        for (int i = height-1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for(int j = 0; j< width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if(grid[j,y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+   
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+
+            if (grid[roundedX, roundedY] != null)
+                return false;
+          
+        }
+
+        return true;
+    }
 }
