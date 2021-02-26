b6b69e99d65703b934cd6ca4aac6e573ed721780
jan <jnkescolano@gmail.com>
2021-02-27 01:30:53 +0800

    game interface + buttons

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource myAudio;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        Invoke("playAudio", 5.0f);
+    }
+
+    public void playAudio()
+    {
+        myAudio.Play();
+    }
+}
