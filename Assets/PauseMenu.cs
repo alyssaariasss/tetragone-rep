6cabd4a1c41dc3eee335170f46d02dc97c7861eb
Shiara-Deudor <58914210+Shiara-Deudor@users.noreply.github.com>
2021-02-28 14:46:53 +0800

    main option

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -42,4 +42,6 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("GameModes");
     }
+
+    
 }
---
+++ Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenu : MonoBehaviour
+{
+    public GameObject optionMenuUI;
+
+    public void Option()
+    {
+        optionMenuUI.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        optionMenuUI.SetActive(false);
+    }
+
+}
